5d83b0e7680b932887a6d99035dc9de3d04354c1
Kaushal Karki <kaushalsinghkarki@gmail.com>
2024-09-02 01:05:52 +0530

    Put request added

--- src/components/AddExpenseForm.js
+++ src/components/AddExpenseForm.js
@@ -6,7 +6,7 @@ import { useUserContext } from '../context/UserContext';
 import { API_BASE_URL, getHeaders } from '../constant';
 import TextField from '@mui/material/TextField';
 
-const AddExpenseForm = ({ onClose, groupId, groupName, onDataSaved,expenseData = null }) => {
+const AddExpenseForm = ({ onClose, groupId, groupName, onDataSaved, expenseData = null }) => {
   const { user, token } = useAuth();
   const userMap = useUserContext();
   const [description, setDescription] = useState(expenseData ? expenseData.description : '');
@@ -18,29 +18,39 @@ const AddExpenseForm = ({ onClose, groupId, groupName, onDataSaved,expenseData =
   const [isSplitEquallyModalOpen, setIsSplitEquallyModalOpen] = useState(false);
   const [loading, setLoading] = useState(true);
   const [users, setUsers] = useState([]);
-  const [selectedUser, setSelectedUser] = useState(expenseData ? expenseData.payer: user ? user.id : null);
-  const [selectedUserName, setSelectedUserName] = useState(expenseData ? userMap[expenseData.payer]: user ? user.name : 'you');
-  const [splitEquallyAmounts, setSplitEquallyAmounts] = useState({});
-  const [selectedSplitUsers, setSelectedSplitUsers] = useState({});
+  const [selectedUser, setSelectedUser] = useState(expenseData ? expenseData.payer : user ? user.id : null);
+  const [selectedUserName, setSelectedUserName] = useState(expenseData ? userMap[expenseData.payer] : user ? user.name : 'you');
+  
+  // Initialize splitEquallyAmounts and selectedSplitUsers based on expenseData
+  const initialSplitEquallyAmounts = expenseData
+    ? Object.fromEntries(expenseData.expense_splits.map((split) => [split.user_id, split.user_amount.toFixed(2)]))
+    : {};
+  const initialSelectedSplitUsers = expenseData
+    ? Object.fromEntries(expenseData.expense_splits.map((split) => [split.user_id, true]))
+    : {};
+  const [splitEquallyAmounts, setSplitEquallyAmounts] = useState(initialSplitEquallyAmounts);
+  const [selectedSplitUsers, setSelectedSplitUsers] = useState(initialSelectedSplitUsers);
+
   useEffect(() => {
     fetchUsers();
   // eslint-disable-next-line
   }, []);
 
   useEffect(() => {
-    if (users.length > 0) {
+    if (users.length > 0 && !expenseData) {
       const initialSplitAmounts = {};
       users.forEach((user) => {
         initialSplitAmounts[user.id] = (amount / users.length).toFixed(2);
       });
       setSplitEquallyAmounts(initialSplitAmounts);
+
       const initialSelectedUsers = {};
       users.forEach((user) => {
         initialSelectedUsers[user.id] = true;
       });
       setSelectedSplitUsers(initialSelectedUsers);
     }
-  }, [users, amount]);
+  }, [users, amount, expenseData]);
 
   useEffect(() => {
     if (expenseData) {
@@ -53,7 +63,7 @@ const AddExpenseForm = ({ onClose, groupId, groupName, onDataSaved,expenseData =
   const fetchUsers = async () => {
     setLoading(true);
     try {
-      const response = await fetch(`${API_BASE_URL}/groups/${groupId}/get_group_users.json`, {headers: getHeaders(token)});
+      const response = await fetch(`${API_BASE_URL}/groups/${groupId}/get_group_users.json`, { headers: getHeaders(token) });
       const data = await response.json();
       if (Array.isArray(data.users)) {
         setUsers(data.users);
@@ -71,13 +81,14 @@ const AddExpenseForm = ({ onClose, groupId, groupName, onDataSaved,expenseData =
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    
     const selectedUsers = Object.keys(selectedSplitUsers).filter((userId) => selectedSplitUsers[userId]);
     const splitAmounts = selectedUsers.map((userId) => ({
       userId,
       amount: splitEquallyAmounts[userId],
     }));
-
-    const expenseData = {
+  
+    const expenseDataPayload = {
       transaction: {
         description,
         amount,
@@ -87,26 +98,31 @@ const AddExpenseForm = ({ onClose, groupId, groupName, onDataSaved,expenseData =
         group_id: groupId,
       },
     };
-
+  
     try {
-      const response = await fetch(`${API_BASE_URL}/expenses`, {
-        method: 'POST',
+      const url = expenseData 
+        ? `${API_BASE_URL}/expenses/${expenseData.id}`
+        : `${API_BASE_URL}/expenses`;
+  
+      const method = expenseData ? 'PUT' : 'POST';  // Use PUT if expenseData exists, otherwise POST
+  
+      const response = await fetch(url, {
+        method,
         headers: getHeaders(token),
-        body: JSON.stringify(expenseData),
+        body: JSON.stringify(expenseDataPayload),
       });
-
+  
       if (response.ok) {
         onDataSaved();
         onClose();
       } else {
-        // Handle error
         const errorData = await response.json();
-        console.error('Error adding expense:', errorData);
-        alert('Error adding expense.');
+        console.error('Error saving expense:', errorData);
+        alert('Error saving expense.');
       }
     } catch (error) {
-      console.error('Error adding expense:', error);
-      alert('Error adding expense.');
+      console.error('Error saving expense:', error);
+      alert('Error saving expense.');
     }
   };
 
@@ -165,10 +181,10 @@ const AddExpenseForm = ({ onClose, groupId, groupName, onDataSaved,expenseData =
       <form className="add-expense-form" onSubmit={handleSubmit}>
         <h2>Add an expense</h2>
         <label>
-           <TextField id="outlined-basic" type='string' value={description} label="Description" variant="outlined" onChange={(e) => setDescription(e.target.value)}/>
+          <TextField id="outlined-basic" type="string" value={description} label="Description" variant="outlined" onChange={(e) => setDescription(e.target.value)} />
         </label>
         <label>
-           <TextField id="outlined-basic" type='integer' value={amount} label="Amount" variant="outlined" onChange={(e) =>  setAmount(e.target.value)}/>
+          <TextField id="outlined-basic" type="integer" value={amount} label="Amount" variant="outlined" onChange={(e) => setAmount(e.target.value)} />
         </label>
         <div className="paid-split-container">
           <p>
@@ -226,14 +242,14 @@ const AddExpenseForm = ({ onClose, groupId, groupName, onDataSaved,expenseData =
             {users.map((user) => (
               <div key={user.id} className="user-item">
                 <div>
-                <input
-                  type="checkbox"
-                  checked={selectedSplitUsers[user.id]}
-                  onChange={() => handleCheckboxChange(user.id)}
-                />
-                {user.name}
+                  <input
+                    type="checkbox"
+                    checked={selectedSplitUsers[user.id]}
+                    onChange={() => handleCheckboxChange(user.id)}
+                  />
+                  {user.name}
                 </div>
-                 <TextField id="outlined-basic" type='integer' value={splitEquallyAmounts[user.id] || ''} label="Amount" variant="outlined" onChange={(e) => handleSplitEquallyAmountChange(user.id, e.target.value)}/>
+                <TextField id="outlined-basic" type="integer" value={splitEquallyAmounts[user.id] || ''} label="Amount" variant="outlined" onChange={(e) => handleSplitEquallyAmountChange(user.id, e.target.value)} />
               </div>
             ))}
           </div>
